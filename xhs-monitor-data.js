fbcf3d5e6adf6626ede86f4073a7e5831f37c54f
louyuhong807-dotcom <louyuhong807@gmail.com>
2026-07-17 20:55:26 +0800

    Add Xiaohongshu comment monitor page

---
+++ xhs-monitor-data.js
@@ -0,0 +1,253 @@
+window.XHS_MONITOR_DATA = {
+  "checked_at": "2026-07-17T12:54:44.798982+00:00",
+  "posts": [
+    {
+      "title": "多人出游✅10房独栋别墅+海景+泳池谁不爱！",
+      "url": "http://xhslink.com/o/3nIIPuJHoTU",
+      "final_url": "https://www.xiaohongshu.com/discovery/item/69e057ae000000001b023ee5?app_platform=android&ignoreEngage=true&app_version=9.33.4&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CBKjGdZjJ8b1rKNhzBg-kFZ8t3xEPm-4RYDGeb97XeTOs%3D&author_share=1&shareRedId=OD9INzk4OkI2NzUyOTgwNjY1OTc1NTY5&apptime=1784193787&share_id=87b224485dc94dfaa5be281d0093c86b&share_channel=copy_link&appuid=69f763690000000002001000&xhsshare=CopyLink",
+      "last_checked": "2026-07-17T12:54:44.798982+00:00",
+      "last_count": 7,
+      "latest_comments": [
+        {
+          "id": "6a58736000000000090155d8",
+          "content": "8月8号什么价格",
+          "nickname": "momo",
+          "xhs_user_id": "5adb676c11be10655b3055a7",
+          "time_ms": 1784181601000,
+          "time": "2026-07-16 14:00:01"
+        },
+        {
+          "id": "6a55ee04000000002a007839",
+          "content": "多少钱，17号晚上",
+          "nickname": "颜值不担当",
+          "xhs_user_id": "5e764baf0000000001002c5f",
+          "time_ms": 1784016388000,
+          "time": "2026-07-14 16:06:28"
+        },
+        {
+          "id": "6a5479ff000000002300c846",
+          "content": "多少钱？",
+          "nickname": "番茄喵宠物店",
+          "xhs_user_id": "5ca070df0000000012017971",
+          "time_ms": 1783921152000,
+          "time": "2026-07-13 13:39:12"
+        },
+        {
+          "id": "6a53409d0000000015008848",
+          "content": "还有么",
+          "nickname": "Sick",
+          "xhs_user_id": "60f250460000000001005e92",
+          "time_ms": 1783840925000,
+          "time": "2026-07-12 15:22:05"
+        },
+        {
+          "id": "6a4321fa000000002300eaa0",
+          "content": "好的",
+          "nickname": "鼎龙湾升旅别墅",
+          "xhs_user_id": "685b5af1000000001d0171fe",
+          "time_ms": 1782784507000,
+          "time": "2026-06-30 09:55:07"
+        }
+      ]
+    },
+    {
+      "title": "鼎龙湾zui美的别墅🏡",
+      "url": "http://xhslink.com/o/nIGivy4HIG",
+      "final_url": "https://www.xiaohongshu.com/discovery/item/6985facc000000001a01eed6?app_platform=android&ignoreEngage=true&app_version=9.33.4&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CBdIHw2OXFxKPIRFmGCe-jBlfgZDvC9u7QCbVzXP6WmDU%3D&author_share=1&shareRedId=OD9INzk4OkI2NzUyOTgwNjY1OTc1NTY5&apptime=1784193932&share_id=a2692baad3614ef28d9692adb574e9ae&share_channel=copy_link&appuid=69f763690000000002001000&xhsshare=CopyLink",
+      "last_checked": "2026-07-17T12:54:44.798982+00:00",
+      "last_count": 12,
+      "latest_comments": [
+        {
+          "id": "6a5872b1000000002a031216",
+          "content": "d d",
+          "nickname": "招财静宝💰",
+          "xhs_user_id": "573b17fd5e87e761a41dca33",
+          "time_ms": 1784181425000,
+          "time": "2026-07-16 13:57:05"
+        },
+        {
+          "id": "6a587223000000002a00620a",
+          "content": "价格",
+          "nickname": "小梅梅哟",
+          "xhs_user_id": "5923094150c4b445df9dd966",
+          "time_ms": 1784181283000,
+          "time": "2026-07-16 13:54:43"
+        },
+        {
+          "id": "6a509b8b000000002902e3f6",
+          "content": "dd",
+          "nickname": "招财静宝💰",
+          "xhs_user_id": "573b17fd5e87e761a41dca33",
+          "time_ms": 1783667596000,
+          "time": "2026-07-10 15:13:16"
+        },
+        {
+          "id": "6a509b6c000000002b02b890",
+          "content": "什么价位",
+          "nickname": "嘉嘉嘉嘉嘉",
+          "xhs_user_id": "5bd1ebd56b58b77784e42936",
+          "time_ms": 1783667564000,
+          "time": "2026-07-10 15:12:44"
+        },
+        {
+          "id": "6a505da2000000002a02d26c",
+          "content": "dd",
+          "nickname": "招财静宝💰",
+          "xhs_user_id": "573b17fd5e87e761a41dca33",
+          "time_ms": 1783651746000,
+          "time": "2026-07-10 10:49:06"
+        }
+      ]
+    },
+    {
+      "title": "湛江鼎龙湾｜步行直达的海边别墅",
+      "url": "http://xhslink.com/o/Wc3e1Mw4ue",
+      "final_url": "https://www.xiaohongshu.com/discovery/item/69c0f555000000001f000995?app_platform=android&ignoreEngage=true&app_version=9.33.4&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CBgLm3SG0WJClIoJL2lkmJGnruvxmQeBtoMX0VUnw4nhM%3D&author_share=1&shareRedId=OD9INzk4OkI2NzUyOTgwNjY1OTc1NTY5&apptime=1784193994&share_id=b143d38854084066814360a1dfdbc8c2&share_channel=copy_link&appuid=69f763690000000002001000&xhsshare=CopyLink",
+      "last_checked": "2026-07-17T12:54:44.798982+00:00",
+      "last_count": 6,
+      "latest_comments": [
+        {
+          "id": "6a576e9c00000000150145e4",
+          "content": "7月周末什么价？",
+          "nickname": "惜",
+          "xhs_user_id": "5eb8bf700000000001000ab2",
+          "time_ms": 1784114845000,
+          "time": "2026-07-15 19:27:25"
+        },
+        {
+          "id": "6a55ed520000000009014204",
+          "content": "八月初，两大一小什么价",
+          "nickname": "英子",
+          "xhs_user_id": "5c552127000000001003abd7",
+          "time_ms": 1784016210000,
+          "time": "2026-07-14 16:03:30"
+        },
+        {
+          "id": "6a5105af000000002b029b55",
+          "content": "8月中什么价位",
+          "nickname": "💪身体健康才是真的赢麻了",
+          "xhs_user_id": "619e05dd0000000021025fe9",
+          "time_ms": 1783694768000,
+          "time": "2026-07-10 22:46:08"
+        },
+        {
+          "id": "6a4b80dd0000000005009aab",
+          "content": "7月25-26/ 六房，16人左右",
+          "nickname": "李绍洪Joe Lee",
+          "xhs_user_id": "5d9190670000000001004150",
+          "time_ms": 1783333085000,
+          "time": "2026-07-06 18:18:05"
+        },
+        {
+          "id": "6a43222e000000002300101e",
+          "content": "别墅最小3房哦",
+          "nickname": "鼎龙湾悦欣海边度假别墅",
+          "xhs_user_id": "64c0bd590000000014036ed2",
+          "time_ms": 1782784558000,
+          "time": "2026-06-30 09:55:58"
+        }
+      ]
+    },
+    {
+      "title": "吴川！海边小院（29号开始空）",
+      "url": "http://xhslink.com/o/52MdMkGiitF",
+      "final_url": "https://www.xiaohongshu.com/discovery/item/69c4f2300000000021039ff6?app_platform=android&ignoreEngage=true&app_version=9.33.4&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CBgb-LPcin1CtAlKmtQbkKUHLOM3VEW-Cd0-WgidH7VAM%3D&author_share=1&shareRedId=OD9INzk4OkI2NzUyOTgwNjY1OTc1NTY5&apptime=1784194127&share_id=1a6ceaedc41a46a8bba4694f47f27193&share_channel=copy_link&appuid=69f763690000000002001000&xhsshare=CopyLink",
+      "last_checked": "2026-07-17T12:54:44.798982+00:00",
+      "last_count": 8,
+      "latest_comments": [
+        {
+          "id": "6a53aeee0000000015005cc8",
+          "content": "17到21号。7个大人6个娃",
+          "nickname": "张惠惠",
+          "xhs_user_id": "5b5a6db3f7e8b93ccb83adb1",
+          "time_ms": 1783869166000,
+          "time": "2026-07-12 23:12:46"
+        },
+        {
+          "id": "6a36a539000000002a02cbf0",
+          "content": "同问",
+          "nickname": "汐瑶",
+          "xhs_user_id": "67277601000000001d02e9f9",
+          "time_ms": 1781966137000,
+          "time": "2026-06-20 22:35:37"
+        },
+        {
+          "id": "6a335bb0000000002803f0d0",
+          "content": "6.27-29什麼價格 湛江鼎龍灣",
+          "nickname": "Qttty➿",
+          "xhs_user_id": "594d381d50c4b46185cd763a",
+          "time_ms": 1781750704000,
+          "time": "2026-06-18 10:45:04"
+        },
+        {
+          "id": "6a2a0550000000002702bfaa",
+          "content": "你好",
+          "nickname": "L_i",
+          "xhs_user_id": "5f86b4e20000000001000071",
+          "time_ms": 1781138769000,
+          "time": "2026-06-11 08:46:09"
+        },
+        {
+          "id": "69e389ad000000001401bc23",
+          "content": "发来",
+          "nickname": "小静吖",
+          "xhs_user_id": "600048c1000000000101fad2",
+          "time_ms": 1776519598000,
+          "time": "2026-04-18 21:39:58"
+        }
+      ]
+    },
+    {
+      "title": "湛江团建 / 家庭游！全海景别墅",
+      "url": "http://xhslink.com/o/1dqExFsf8dW",
+      "final_url": "https://www.xiaohongshu.com/discovery/item/691d8f08000000001e004cc4?app_platform=android&ignoreEngage=true&app_version=9.33.4&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CBQWIbkKpWMwrjfkvaAw7CNMAnITM8ly_wZ1OobQrKfi8%3D&author_share=1&shareRedId=OD9INzk4OkI2NzUyOTgwNjY1OTc1NTY5&apptime=1784194210&share_id=6cae60846bac4fa2b5b503015a607ad0&share_channel=copy_link&appuid=69f763690000000002001000&xhsshare=CopyLink",
+      "last_checked": "2026-07-17T12:54:44.798982+00:00",
+      "last_count": 6,
+      "latest_comments": [
+        {
+          "id": "6a59d8660000000005015154",
+          "content": "可以住多少人，连住7天什么价",
+          "nickname": "……勿忘心安",
+          "xhs_user_id": "5fbddf28000000000100b782",
+          "time_ms": 1784272999000,
+          "time": "2026-07-17 15:23:19"
+        },
+        {
+          "id": "6a4f1f11000000002901a2f8",
+          "content": "4晚什么价格？",
+          "nickname": "MENCHING",
+          "xhs_user_id": "5ee1fbf4000000000101f955",
+          "time_ms": 1783570194000,
+          "time": "2026-07-09 12:09:54"
+        },
+        {
+          "id": "69da593300000000150036a3",
+          "content": "怎么联系？",
+          "nickname": "大妞小妞🌸",
+          "xhs_user_id": "5d7712ee000000000100076b",
+          "time_ms": 1775917364000,
+          "time": "2026-04-11 22:22:44"
+        },
+        {
+          "id": "69d9b95f000000003502006c",
+          "content": "多少",
+          "nickname": "'WANG®",
+          "xhs_user_id": "580689f182ec39602a49fb86",
+          "time_ms": 1775876447000,
+          "time": "2026-04-11 11:00:47"
+        },
+        {
+          "id": "694230b50000000016026ca3",
+          "content": "春节5天收费多少",
+          "nickname": "丹丹",
+          "xhs_user_id": "5f53aad70000000001001806",
+          "time_ms": 1765945526000,
+          "time": "2025-12-17 12:25:26"
+        }
+      ]
+    }
+  ],
+  "new_comments": [],
+  "alert_history": [],
+  "errors": []
+};
